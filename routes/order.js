fefde1cc1ee701f08c59ee815020d79e1876587e
diyaa_code <zdiyaa23code@gmail.com>
2023-09-01 17:57:14 +0100

    update9012023

--- routes/order.js
+++ routes/order.js
@@ -51,14 +51,18 @@ router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
 });
 
 //GET USER ORDERS
-router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
-  try {
-    const orders = await Order.find({ userId: req.params.userId });
-    res.status(200).json(orders);
-  } catch (err) {
-    res.status(500).json(err);
+router.get(
+  "/find/:userId",
+  //verifyTokenAndAuthorization,
+  async (req, res) => {
+    try {
+      const orders = await Order.find({ userId: req.params.userId });
+      res.status(200).json(orders);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
-});
+);
 
 // //GET ALL
 
